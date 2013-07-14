0f19fa15ee1cd2393e8ea2a429ddb5a0723a25a7
Ori Pekelman <ori@pekelman.com>
2013-07-14 18:16:32 +0200

    update to new api

--- examples/index.rb
+++ examples/index.rb
@@ -1,14 +1,13 @@
 require_relative '../lib/oss_rb'
 require 'securerandom'
 index = Oss::Index.new("test_oss_rb")
-
 index.delete!
-index.create!
+indexes = index.create('EMPTY_INDEX')
 puts index.list
 
 (1..15).each do |i|
   id = SecureRandom.uuid
-  doc = Oss::Document.new("en", i)  
+  doc = Oss::Document.new 
   doc.add_field("user", "jane#{i}")
   doc.add_field("user", "john#{i}")
   doc.add_field("url", "http://myserver.com/#{id}")
